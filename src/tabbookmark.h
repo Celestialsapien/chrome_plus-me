4c1e4f30496f35cc7e8296394e4c505363762de4
Celestialsapien <145863291+Celestialsapien@users.noreply.github.com>
2025-04-22 22:08:40 +0800

    Update tabbookmark.h

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -251,7 +251,7 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
       PMOUSEHOOKSTRUCTEX pwheel = (PMOUSEHOOKSTRUCTEX)lParam;
       // 惯性滚动参数
       static float inertia_speed = 0;
-      static const float DECELERATION = 0.5f; // 衰减系数
+      static const float DECELERATION = 0.8f; // 衰减系数
       static UINT_PTR inertia_timer = 0;
       
       // 获取原始滚动量并翻倍
